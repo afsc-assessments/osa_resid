70b049d92821ba1ae6591fc333b1231e098adc9b
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2023-07-07 11:07:38 -0700

    add data.table and maditr dependencies

--- R/osa_run_ss.R
+++ R/osa_run_ss.R
@@ -6,19 +6,19 @@
 OSA_run_SS<-function(model="Model23.1.0",ages=0:12, fleet=2, sx=1,stck='EBS_COD',surv='EBSSHELF'){
    require(data.table)
    mods1<-r4ss::SSgetoutput(dirvec=model)
-   age<-data.table(mods1[[1]]$agedbase[,c(1,6,13,16:19)])[Bin%in%ages & Fleet==fleet & Sex==sx]
-   age<-data.table(melt(age,c('Yr','Fleet','Sex','Bin')))
+   age<-data.table::data.table(mods1[[1]]$agedbase[,c(1,6,13,16:19)])[Bin%in%ages & Fleet==fleet & Sex==sx]
+   age<-data.table::data.table(melt(age,c('Yr','Fleet','Sex','Bin')))
    o<-age[variable=='Obs']
-   o<-dcast(o,Yr~Bin)
+   o<-maditr::dcast(o,Yr~Bin)
    p<-age[variable=='Exp']
-   p<-dcast(p,Yr~Bin)
+   p<-maditr::dcast(p,Yr~Bin)
    pearson<-age[variable=='Pearson']
-   pearson<-dcast(pearson,Yr~Bin)
+   pearson<-maditr::dcast(pearson,Yr~Bin)
    years<-o$Yr
    o <- as.matrix(o[,-1])
    p <- as.matrix(p[,-1])
    pearson <- as.matrix(pearson[,-1])
-   Neff<-round(data.table(mods1[[1]]$agedbase)[Bin==ages[1] & Sex==sx]$effN)
+   Neff<-round(data.table::data.table(mods1[[1]]$agedbase)[Bin==ages[1] & Sex==sx]$effN)
    plot_osa_comps(o,p, pearson, ages=ages, years=years, Neff=Neff, stock=paste("A",sx,stck,model,sep="_"), survey=surv)
   }
 
@@ -26,19 +26,19 @@ OSA_run_SS<-function(model="Model23.1.0",ages=0:12, fleet=2, sx=1,stck='EBS_COD'
 
   OSA_run_SS_length<-function(model="Model23.1.0",lengths=seq(3.5,75.5,by=1), fleet=2, sx=1, stck='EBS_COD',surv='EBSSHELF'){
    mods1<-r4ss::SSgetoutput(dirvec=model)
-   age<-data.table(mods1[[1]]$lendbase[,c(1,6,13,16:19)])[Bin%in%lengths & Fleet==fleet &Sex==sx]
-   age<-data.table(melt(age,c('Yr','Fleet','Sex','Bin')))
+   age<-data.table::data.table(mods1[[1]]$lendbase[,c(1,6,13,16:19)])[Bin%in%lengths & Fleet==fleet &Sex==sx]
+   age<-data.table::data.table(melt(age,c('Yr','Fleet','Sex','Bin')))
    o<-age[variable=='Obs']
-   o<-dcast(o,Yr~Bin)
+   o<-maditr::dcast(o,Yr~Bin)
    p<-age[variable=='Exp']
-   p<-dcast(p,Yr~Bin)
+   p<-maditr::dcast(p,Yr~Bin)
    pearson<-age[variable=='Pearson']
-   pearson<-dcast(pearson,Yr~Bin)
+   pearson<-maditr::dcast(pearson,Yr~Bin)
    years<-o$Yr
    o <- as.matrix(o[,-1])
    p <- as.matrix(p[,-1])
    pearson <- as.matrix(pearson[,-1])
-   Neff<-round(data.table(mods1[[1]]$lendbase)[Bin==lengths[1]& Fleet==fleet & Sex==sx]$effN)
+   Neff<-round(data.table::data.table(mods1[[1]]$lendbase)[Bin==lengths[1]& Fleet==fleet & Sex==sx]$effN)
    plot_osa_comps(o,p, pearson, ages=lengths, years=years, Neff=Neff, stock=paste("L",sx,stck,model,sep="_"), survey=surv)
   }
 
